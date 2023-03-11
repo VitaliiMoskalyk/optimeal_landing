c1699221ac4617d76478da1a2327a523ec65d8dc
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2023-03-11 23:32:23 +0200

    new-Line section

---
+++ js/slider.js
@@ -0,0 +1,60 @@
+// MICRO-SLIDER with Autoplay + Hover Pause
+// Powered by:
+// Micro-Slider JS by LGSE https://github.com/lgse/micro-slider
+// Hammer JS gesture library
+// https://github.com/hammerjs/hammer.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  //===== MICRO-SLIDER begin
+  const __ms = document.querySelector(".micro-slider");
+  const __msSlider = new MicroSlider(__ms, {
+    indicators: true,
+    indicatorText: "",
+  });
+  const hammer = new Hammer(__ms);
+  const __msTimer = 3000;
+  let __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+
+  //detect mouseenter event
+  __ms.onmouseenter = function (e) {
+    clearInterval(__msAutoplay);
+    console.log(e.type + " mouse detected");
+  };
+
+  //detect mouseleave event
+  __ms.onmouseleave = function (e) {
+    clearInterval(__msAutoplay);
+    __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+    console.log(e.type + " mouse detected");
+  };
+
+  //detect mouseclick event
+  __ms.onclick = function (e) {
+    clearInterval(__msAutoplay);
+    console.log(e.type + " mouse detected");
+  };
+
+  //detect gesture tap event with hammer js library
+  hammer.on("tap", function (e) {
+    clearInterval(__msAutoplay);
+    console.log(e.type + " gesture detected");
+  });
+
+  //detect gesture swipe event with hammer js library
+  hammer.on("swipe", function (e) {
+    clearInterval(__msAutoplay);
+    __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+    console.log(e.type + " gesture detected");
+  });
+
+  let slideLink = document.querySelectorAll(".slider-item");
+  if (slideLink && slideLink !== null && slideLink.length > 0) {
+    slideLink.forEach((el) =>
+      el.addEventListener("click", (e) => {
+        e.preventDefault();
+      })
+    );
+  }
+
+  //===== MICRO-SLIDER end
+});
